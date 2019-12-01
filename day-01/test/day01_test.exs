a8e4652d37d01a6938700e0ea0fa3ad6adf866ee
Anderson Konzen <anderson@anderkonzen.com>
2019-12-01 16:57:46 -0300

    feat: day 1, part 2

--- day-01/test/day01_test.exs
+++ day-01/test/day01_test.exs
@@ -9,4 +9,10 @@ defmodule Day01Test do
     assert Day01.fuel(1969) == 654
     assert Day01.fuel(100_756) == 33583
   end
+
+  test "fuel_recursive/1 returns required fuel for given mass and fuel" do
+    assert Day01.fuel_recursive(14) == 2
+    assert Day01.fuel_recursive(1969) == 966
+    assert Day01.fuel_recursive(100_756) == 50346
+  end
 end
